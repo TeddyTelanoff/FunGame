749fe98cc7537c0a546c5d063f770c01723ae35f
Treidex <treidexsemextetry@gmail.com>
2021-03-04 21:08:19 -0800

    strange

--- src/Main.c
+++ src/Main.c
@@ -1,21 +1,47 @@
+#include <stdio.h>
 #include <Windows.h>
 
 #define PI 3.1415926535
 
+void $PrintRECT(const RECT *rect)
+{
+	printf("%li, %li, %li, %li\n", rect->left, rect->top, rect->right, rect->bottom);
+}
 
+SIZE_T FunGame$GetRECTWidth(const RECT *rect)
+{ return rect->right - rect->left; }
+
+SIZE_T FunGame$GetRECTHeight(const RECT *rect)
+{ return rect->bottom - rect->top; }
 
 int main()
 {
 	HWND console = GetConsoleWindow();
 	HDC dc = GetDC(console);
 
-	RECT consoleRect;
-	GetWindowRect(console, &consoleRect);
-	for (UINT32 y = 0; y < consoleRect.right; y++)
-		for (UINT32 x = 0; x < consoleRect.bottom; x++)
-			SetPixel(dc, x, y, 0x00FF00);
+	HBRUSH brush = CreateSolidBrush(0x0000FF);
+	HBRUSH pBrush = SelectObject(dc, brush);
 
-    ReleaseDC(console, dc);
+	while (TRUE)
+	{
+		if (GetAsyncKeyState('Q') & 0x8000)
+			break;
 
+		RECT consoleRect;
+		GetWindowRect(console, &consoleRect);
+		$PrintRECT(&consoleRect);
 
+		//POINT verts[] =
+		//{
+		//	{ 5, 5 },
+		//	{ FunGame$GetRECTWidth(&consoleRect) - 5, 5 },
+		//	{ FunGame$GetRECTWidth(&consoleRect) - 5, FunGame$GetRECTHeight(&consoleRect) - 5 },
+		//	{ 5, FunGame$GetRECTHeight(&consoleRect) - 5 },
+		//};
+		//Polygon(dc, verts, sizeof(verts) / sizeof(*verts));
+	}
+
+	SelectObject(dc, pBrush);
+	DeleteObject(pBrush);
+	ReleaseDC(console, dc);
 }
